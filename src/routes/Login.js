a16be9bbdf869390ccb78f7ddb0ae90c4dbe0e48
unknown <matthewacha@gmail.com>
2018-05-16 10:07:13 +0300

    [feature] Add login page

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -4,29 +4,29 @@ import './static/staticHome.css';
 export default () => (
     <div>
         <nav>
-		<div class="navibar">
+		<div className="navibar">
 			<ul>
-				<li><a class="nav-logo" href="Signup">Book-A-Meal</a></li>
-				<li id="right-but"><a class="nav-logo" href="Signup">Signup</a></li>
+				<li><a className="nav-logo" href="Signup">Book-A-Meal</a></li>
+				<li id="right-but"><a className="nav-logo" href="Signup">Signup</a></li>
 			</ul>
 	     </div>
 	</nav>
 	
-	<div class = "background">
-		<div class="input-forms">
-			<div class="h2">
+	<div className = "background">
+		<div className="input-forms">
+			<div className="h2">
 			<h2>Login</h2>
 			</div>
 			<form action="admin-dash.html" href="customerHome.html">
-				<div class="form-inputs">
+				<div className="form-inputs">
 					<div>
-					  <label class="label">Email</label><br/>
-					  <input type="email" class="form-control" placeholder="you@email.com" required/><br/><br/>
-				      <label class="label">Password</label><br/>
-					  <input type="password" class="form-control" placeholder="*********" required/><br/>
+					  <label className="label">Email</label><br/>
+					  <input type="email" className="form-control" placeholder="you@email.com" required/><br/><br/>
+				      <label className="label">Password</label><br/>
+					  <input type="password" className="form-control" placeholder="*********" required/><br/>
 					</div><br/>
 					<div>
-					  <button class='submit-button' type="submit">Submit</button>
+					  <button className='submit-button' type="submit">Submit</button>
 					</div>
 				</div>
 			</form>
