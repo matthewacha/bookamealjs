535c8bfc9e93c38d01b644dcd152a72234a1d15d
unknown <matthewacha@gmail.com>
2018-05-22 11:16:23 +0300

    [feature] Add actions for authentication

---
+++ src/actions/credActions.js
@@ -0,0 +1,51 @@
+import { SIGN_USER, LOGIN_USER } from './types';
+import { push } from 'react-router-redux';
+import history from '../components/history';
+
+export const signUp=(signData)=>dispatch => {
+    if (signData){
+
+        const options = {
+            method:'POST',
+            body:signData, 
+            headers:{
+                'Content-Type':'application/json'}};
+       fetch(`http://127.0.0.1:5000/api/v2/auth/signup`,options)
+       .then (result => result.json())
+       .then (data=> dispatch(
+        {
+            type: 'SIGN_USER',
+            payload:data
+        }
+    
+    ))
+    
+    }
+}
+
+export const logIn=(loginData)=>dispatch => {
+    if (loginData){
+
+        const options = {
+            method:'POST',
+            body:loginData, 
+            headers:{
+                'Content-Type':'application/json'}};
+       fetch(`http://127.0.0.1:5000/api/v2/auth/login`,options)
+       .then (result => result.json())
+       .then (data=> dispatch(
+        {
+            type: 'LOGIN_USER',
+            token:data
+        }
+    ))
+    
+    }
+}
+
+export const redirectLogin = ()=>dispatch=>{
+    //console.log(history.push("/login"));
+    //dispatch(history.push('/login'));
+    history.push('/login');
+}
+
---
+++ src/actions/types.js
@@ -0,0 +1,2 @@
+export const SIGN_USER = 'SIGN_USER';
+export const LOGIN_USER = 'LOGIN_USER';
