bd6195120aaaa36fdbdf11137a0f965b4179e4d4
unknown <matthewacha@gmail.com>
2018-05-22 11:14:38 +0300

    [feature] Add history, store and Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import Index from './routes/index';
+import Index from './components';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import PropTypes from 'prop-types';
+import bookMeal from './store';
+import history from './components/history';
 
+export default class App extends React.Component {
+    render () {
+        return (
+        <Provider store = {bookMeal} history = {history}>
+                <BrowserRouter>
+                        <Index/>
+                </BrowserRouter>
+        </Provider>)
+        }
+}
 
-export default () => {
-    return (
-        <BrowserRouter>
-                <Index/>
-        </BrowserRouter>)
-};
+App.propTypes = {
+        store:PropTypes.object.isRequired
+}
+
