23f149f0e3e0ee1b8eee64297044bc78c01609ad
unknown <matthewacha@gmail.com>
2018-05-22 11:18:33 +0300

    [feature] Configure store

---
+++ src/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import mainReducer from './reducers';
+
+const middleware = [thunk];
+const bookMeal = createStore(mainReducer, {},applyMiddleware(...middleware));
+
+export default  bookMeal;
