7a53a58a7914568d4bc1ec5c34c5d5f7e0709d25
unknown <matthewacha@gmail.com>
2018-06-05 23:31:42 +0300

    [feature] Add types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,23 @@
 export const SIGN_USER = 'SIGN_USER';
 export const LOGIN_USER = 'LOGIN_USER';
+export const VERIFY_LOGIN = 'VERIFY_LOGIN';
+
+//Admin types
+export const ADMIN_SIGNUP = 'ADMIN_SIGNUP';
+export const ADMIN_LOGIN = 'ADMIN_LOGIN';
+
+export const FETCH_ORDERS = 'FETCH_ORDERS';
+export const FETCH_MEALS = 'FETCH_MEALS';
+
+export const ADD_MEAL = 'ADD_MEAL';
+export const GET_MEAL = 'GET_MEAL';
+export const EDIT_MEAL = 'EDIT_MEAL';
+export const DELETE_MEAL = 'DELETE_MEAL';
+export const ADD_TO_MENU = 'ADD_TO_MENU';
+export const GET_MENU = 'GET_MENU';
+export const GET_MENUS = 'GET_MENUS';
+
+export const DELETE_MENU_MEAL = 'DELETE_MENU_MEAL';
+
+// helper types
+export const EDIT_STATE = 'EDIT_STATE';
