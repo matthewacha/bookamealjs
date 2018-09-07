092adf91bdad258f51e275915f105d344b8a057e
Matthew Wacha <30747298+matthewacha@users.noreply.github.com>
2018-09-07 18:26:35 +0300

    Revert "feat(user login)Edit user login"
    This reverts commit 4c40470698105fc2f6a43856a2d42f7e15993238.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Index from './components';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-
 import PropTypes from 'prop-types';
 import bookMeal from './store';
 import history from './components/history';
@@ -12,7 +11,6 @@ export default class App extends React.Component {
         return (
         <Provider store = {bookMeal} history = {history}>
                 <BrowserRouter>
-                       
                         <Index/>
                 </BrowserRouter>
         </Provider>)
